2b6c6afa3b428e40c94ab887b2554ed2d3a047b8
Ula-Lysenko <highspirit@ukr.net>
2023-05-25 23:59:54 +0300

    updates

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".carousel").slick({
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
+});
